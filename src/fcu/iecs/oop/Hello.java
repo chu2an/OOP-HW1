5d2499fdd11d2bf046918afd85b0f82533a55352
chuan <user@FE04>
2016-09-30 09:26:15 +0800

    Frozen Lyrics v2.0

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		
-		String lyrics = "Lete it go! Let it go! Caannot hold it back anymore";
+		String lyrics = "Let her go! Let her go! Can't hold her back anymore";
+		System.out.println(lyrics);
 	}
 
 }
