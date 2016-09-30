f31747e142ffdcbc7078fed752aa65c3b625e68d
chuan <user@FE04>
2016-09-30 09:21:00 +0800

    Frozen Lyrics

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -3,8 +3,8 @@ package fcu.iecs.oop;
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello OOP");
+		
+		String lyrics = "Lete it go! Let it go! Caannot hold it back anymore";
 	}
 
 }
